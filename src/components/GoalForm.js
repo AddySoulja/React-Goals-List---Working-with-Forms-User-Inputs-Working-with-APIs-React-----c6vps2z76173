634dbd586d5e3e74ec87ace3601676a381496b0b
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-05-03 13:48:41 +0530

    Build

--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -1,44 +1,28 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
+const GoalForm = ({ setAllGoals }) => {
+  const goalRef = useRef();
+  const byRef = useRef();
 
-const initialForm = {
-  goal: "",
-  by: "",
-};
-const GoalForm = ({ allGoals, setAllGoals }) => {
-  const [formData, setFormData] = useState(initialForm);
-  // const goalRef = useRef();
-  // const byRef = useRef();
   const handleSubmit = (e) => {
     e.preventDefault();
-    setAllGoals([
-      ...allGoals,
-      <li key={allGoals.length + 1}>
-        My goal is to {formData.goal}, by {formData.by}.
-      </li>,
-    ]);
-    setFormData(initialForm);
+    setAllGoals((goals) => {
+      return [
+        ...goals,
+        <li key={allGoals.length + 1}>
+          My goal is to {goalRef.current.value}, by {byRef.current.value}.
+        </li>,
+      ];
+    });
+    goalRef.current.value = "";
+    byRef.current.value = "";
   };
 
   return (
     <>
       <h1>My Goals</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="goal"
-          placeholder="Goal..."
-          // ref={goalRef}
-          value={formData.goal}
-          onChange={(e) => setFormData({ ...formData, goal: e.target.value })}
-        />
-        <input
-          type="text"
-          name="by"
-          placeholder="By..."
-          // ref={byRef}
-          value={formData.by}
-          onChange={(e) => setFormData({ ...formData, by: e.target.value })}
-        />
+        <input type="text" name="goal" placeholder="Goal..." ref={goalRef} />
+        <input type="text" name="by" placeholder="By..." ref={byRef} />
         <button type="submit">Add</button>
       </form>
     </>
